da376746bd1213c25c87046b4c3458ab08d69d16
joselacruz <35085600+joselacruz@users.noreply.github.com>
2024-06-10 10:04:26 -0500

    Update project list and reorganize

--- src/app/api.js
+++ src/app/api.js
@@ -1,24 +1,5 @@
 const projects = [
   {
-    title: 'Batatabit',
-    content:
-      'Building a website with a mobile-optimized design and development approach before tackling desktop versions using "Mobile first".',
-    group: 'vanillajs',
-    img: 'https://i.ibb.co/yn9tkbp/batatabit.png',
-
-    code: 'https://github.com/joselacruz/mobile-frist',
-    live: 'https://joselacruz.github.io/mobile-frist',
-  },
-  {
-    title: 'Todo List',
-    content:
-      'I use React to create a responsive interface that allows adding tasks. I use React components and concepts to add, remove, and mark tasks as complete.',
-    group: 'react',
-    img: 'https://i.ibb.co/s27Qf36/todo-list.png',
-    code: 'https://github.com/joselacruz/Todo-list',
-    live: 'https://joselacruz.github.io/Todo-list',
-  },
-  {
     title: 'Rank Pelis',
     content:
       'Implementation of a movie site using HTML, CSS, and JavaScript that consume the API (themoviedb) and show the movies organized by category and popularity.',
@@ -26,6 +7,15 @@ const projects = [
     img: 'https://i.ibb.co/6F32tkS/rank-pelis.png',
     code: 'https://github.com/joselacruz/Rank-pelis',
     live: 'https://joselacruz.github.io/Rank-pelis',
+  },
+  {
+    title: 'Raking Radios',
+    content:
+      'Website that displays radio stations using the Radio Info API and saves user data to Firebase.',
+    group: 'react',
+    img: 'https://i.ibb.co/0jJGpjL/radios.png',
+    code: 'https://github.com/joselacruz/ranking-radios',
+    live: 'https://ranking-radios.netlify.app',
   },
   {
     title: 'Mood Tracker',
@@ -46,15 +36,6 @@ const projects = [
     live: 'https://easy-sale-register.netlify.app',
   },
   {
-    title: 'Raking Radios',
-    content:
-      'Website that displays radio stations using the Radio Info API and saves user data to Firebase.',
-    group: 'react',
-    img: 'https://i.ibb.co/0jJGpjL/radios.png',
-    code: 'https://github.com/joselacruz/ranking-radios',
-    live: 'https://ranking-radios.netlify.app',
-  },
-  {
     title: 'Rifa Live',
     content:
       'Web application to sell raffles of 200 numbers with purchase function and notifications via WhatsApp.',
@@ -62,6 +43,25 @@ const projects = [
     img: 'https://i.ibb.co/nnjsPmc/rifa-live.png',
     code: 'https://github.com/joselacruz/rifaLive',
     live: 'https://rifalive.netlify.app',
+  },
+  {
+    title: 'Todo List',
+    content:
+      'I use React to create a responsive interface that allows adding tasks. I use React components and concepts to add, remove, and mark tasks as complete.',
+    group: 'react',
+    img: 'https://i.ibb.co/s27Qf36/todo-list.png',
+    code: 'https://github.com/joselacruz/Todo-list',
+    live: 'https://joselacruz.github.io/Todo-list',
+  },
+  {
+    title: 'Batatabit',
+    content:
+      'Building a website with a mobile-optimized design and development approach before tackling desktop versions using "Mobile first".',
+    group: 'vanillajs',
+    img: 'https://i.ibb.co/yn9tkbp/batatabit.png',
+
+    code: 'https://github.com/joselacruz/mobile-frist',
+    live: 'https://joselacruz.github.io/mobile-frist',
   },
   {
     title: 'Recipe Blog',
